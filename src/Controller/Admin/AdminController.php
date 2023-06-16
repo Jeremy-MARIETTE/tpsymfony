b82b5ba3ebf7a9bf58585bf8d3533d5cca4a89ab
Jérémy MARIETTE <j-mariette@protonmail.com>
2023-06-16 23:23:43 +0200

    afficher données rondes

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Ronde;
 use App\Entity\Entreprise;
 use App\Repository\UserRepository;
+use App\Repository\RondeRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,31 +16,24 @@ class AdminController extends AbstractController
 
     private $entityManager;
 
-    public function __construct(EntityManagerInterface $entityManager)
+    public function __construct(EntityManagerInterface $entityManager, RondeRepository $rondeRepository)
     {
         $this->entityManager = $entityManager;
+        $this->rondeRepository = $rondeRepository;
     }
     
     #[Route('/admin', name: 'app_admin')]
     public function index(): Response
     {
-        /*
-        return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
-        ]);
-
-        
-          $user = $this->getUser()->getId();
       
-        return $this->render('entreprise_controller_crud/index.html.twig', [
-            'entreprises' => $entrepriseRepository->findBy(['idGerant'=> $user]),
-        ]);
-         */
 
-         $userId = $this->getUser()->getId(); // Assurez-vous que cette méthode retourne l'ID du gérant actuel
+        $userId = $this->getUser()->getId(); // Assurez-vous que cette méthode retourne l'ID du gérant actuel
 
         $entrepriseRepository = $this->entityManager->getRepository(Entreprise::class);
         $entreprise = $entrepriseRepository->findOneBy(['idGerant' => $userId]);
+
+        $rondeRepository = $this->entityManager->getRepository(Ronde::class);
+        $ronde = $rondeRepository->findAll();
 
     // Maintenant, vous avez l'objet Entreprise correspondant à l'ID du gérant
     // Vous pouvez l'utiliser dans votre template ou faire ce que vous voulez avec
@@ -46,6 +41,7 @@ class AdminController extends AbstractController
     return $this->render('admin/index.html.twig', [
         'controller_name' => 'AdminController',
         'entreprise' => $entreprise, // Passez l'objet Entreprise au template
+        'rondes' => $ronde
     ]);
     }
 }
