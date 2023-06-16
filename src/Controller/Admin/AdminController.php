c6ee44f008d8b98206faf83bf7d3fa3de2bb6727
Jérémy MARIETTE <j-mariette@protonmail.com>
2023-06-17 00:29:19 +0200

    choix liste par site

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -2,11 +2,14 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Site;
 use App\Entity\Ronde;
 use App\Entity\Entreprise;
+use App\Repository\SiteRepository;
 use App\Repository\UserRepository;
 use App\Repository\RondeRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -16,14 +19,15 @@ class AdminController extends AbstractController
 
     private $entityManager;
 
-    public function __construct(EntityManagerInterface $entityManager, RondeRepository $rondeRepository)
+    public function __construct(EntityManagerInterface $entityManager, RondeRepository $rondeRepository, SiteRepository $siteRepository)
     {
         $this->entityManager = $entityManager;
         $this->rondeRepository = $rondeRepository;
+        $this->siteRepository = $siteRepository;
     }
     
     #[Route('/admin', name: 'app_admin')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
       
 
@@ -32,8 +36,25 @@ class AdminController extends AbstractController
         $entrepriseRepository = $this->entityManager->getRepository(Entreprise::class);
         $entreprise = $entrepriseRepository->findOneBy(['idGerant' => $userId]);
 
+        $siteRepository = $this->entityManager->getRepository(Site::class);
+        $site = $siteRepository->findAll();
+
+
         $rondeRepository = $this->entityManager->getRepository(Ronde::class);
-        $ronde = $rondeRepository->findAll();
+
+        //réccupèrer l'option choisi par l'utilisateur
+
+        //handleRequest() permet de récupérer les données du formulaire
+   
+        var_dump($request->request->get('site'));
+        
+        $choix = $request->request->get('site');
+      if($choix != null){
+        $ronde = $rondeRepository->findBy(['site' => $choix]);
+        }else{
+            $ronde = $rondeRepository->findBy(['site' => '0']);
+        }
+      
 
     // Maintenant, vous avez l'objet Entreprise correspondant à l'ID du gérant
     // Vous pouvez l'utiliser dans votre template ou faire ce que vous voulez avec
@@ -41,7 +62,8 @@ class AdminController extends AbstractController
     return $this->render('admin/index.html.twig', [
         'controller_name' => 'AdminController',
         'entreprise' => $entreprise, // Passez l'objet Entreprise au template
-        'rondes' => $ronde
+        'rondes' => $ronde,
+        'sites' => $site
     ]);
     }
 }
